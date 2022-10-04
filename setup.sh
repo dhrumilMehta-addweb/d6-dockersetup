5fc00744b31cb2dac6154a9badf1b82aec1e16da
dhrumilMehta-addweb <88422120+dhrumilMehta-addweb@users.noreply.github.com>
2022-10-04 13:58:17 +0530

    initial commit

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+docker compose up -d
+sudo chown -R $USER:www-data ./drupal
+
+cd drupal  && git clone https://github.com/d6lts/drupal.git .
+sudo cp ./sites/default/default.settings.php ./sites/default/settings.php
+sudo chown -R $USER:www-data *
+sudo chmod -R 775 *
