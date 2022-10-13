266a720dbd8f62a4859bda38272cb1bd90153f86
dhrumilMehta-addweb <88422120+dhrumilMehta-addweb@users.noreply.github.com>
2022-10-13 16:08:50 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,25 @@
-#!/bin/sh
-docker-compose up -d
-sudo chown -R $USER:www-data ./drupal
+# based on https://hub.docker.com/r/wodby/drupal6/dockerfile
+FROM orsolin/docker-php-5.3-apache
 
-cd drupal  && git clone https://github.com/d6lts/drupal.git .
-sudo cp ./sites/default/default.settings.php ./sites/default/settings.php
-sudo chown -R $USER:www-data *
-sudo chmod -R 775 *
+
+
+# RUN a2enmod rewrite
+
+# install the PHP extensions we need
+RUN apt-get update -y && apt-get install -y libpng12-dev libjpeg-dev --force-yes
+
+RUN apt-get update && apt-get install -y  --force-yes \
+		libfreetype6-dev \
+		libjpeg62-turbo-dev \
+		libpng-dev \
+	&& docker-php-ext-configure gd  \
+	&& docker-php-ext-install  gd  mbstring pdo pdo_mysql  mysql mysqli 
+
+#--with-freetype --with-jpeg
+# RUN docker-php-ext-configure gd --with-png-dir=/usr --with-jpeg-dir=/usr \
+#     && docker-php-ext-install gd mbstring pdo pdo_mysql pdo_pgsql mysql mysqli 
+
+
+
+
+WORKDIR /var/www/html
