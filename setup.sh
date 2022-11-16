f92986f937a09dd27013c2e8cdc5fca7bc1c6f6e
is-jfulmali <jfulmali@synergies.co>
2022-11-16 11:41:25 +0530

    edited build script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 docker-compose up -d
 sudo chown -R $USER:www-data ./drupal
+read -s -p "Enter Git-UserName: " gituser 
+echo
+read -s -p "Enter Git-Token: " gittoken 
 
-cd drupal  && git clone https://github.com/d6lts/drupal.git .
+
+cd drupal  && git clone https://$gituser:$gittoken@github.com/nightlifemedia/UNL_Drupal.git .
 sudo cp ./sites/default/default.settings.php ./sites/default/settings.php
 sudo chown -R $USER:www-data *
 sudo chmod -R 775 *
